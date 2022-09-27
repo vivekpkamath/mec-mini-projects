1c3dfd493f229fa9ab32743b19fc40e0c037a2de
vivekpkamath <vivek.p.kamath@gmail.com>
2022-09-26 20:51:42 -0700

    Finished training code for topic model

--- capstone/model/TopicModel.py
+++ capstone/model/TopicModel.py
@@ -37,20 +37,24 @@ class TopicModel(ModelInterface):
         #nltk.download('punkt')
         #nltk.download('stopwords')
         #nltk.download('wordnet')
+        #nltk.download('omw-1.4')
+        
         #load all documents from database
-        document_df = self._db_io.read_to_dataframe('select * from documents')
+        document_df = self._db_io.read_to_dataframe('select * from documents;')
         self._norm_documents = self.__normalize_corpus(document_df['body'].to_list())
         #create bigram phrases
-
-        bigram = gensim.models.Phrases(self._norm_documents, min_count=20, threshold=20, delimiter=b'_') # higher threshold fewer phrases.
+        #print(self._norm_documents)
+        # higher threshold fewer phrases.
+        #bigram = gensim.models.Phrases(self._norm_documents, min_count=20, threshold=20, delimiter= b'_') 
+        bigram = gensim.models.Phrases(self._norm_documents, min_count=20, threshold=20) 
 
         bigram_model = gensim.models.phrases.Phraser(bigram)
         print(bigram_model[self._norm_documents[0]][:50])
 
-        norm_corpus_bigrams = [bigram_model[doc] for doc in norm_papers]
+        self._norm_corpus_bigrams = [bigram_model[doc] for doc in self._norm_documents]
 
         # Create a dictionary representation of the documents.
-        self._dictionary = gensim.corpora.Dictionary(norm_corpus_bigrams)
+        self._dictionary = gensim.corpora.Dictionary(self._norm_corpus_bigrams)
         print('Sample word to number mappings:', list(self._dictionary.items())[:15])
         print('Total Vocabulary Size:', len(self._dictionary))
 
@@ -59,35 +63,35 @@ class TopicModel(ModelInterface):
         print('Total Vocabulary Size:', len(self._dictionary))
 
         #Create BOW
-        self._bow_corpus = [self._dictionary.doc2bow(text) for text in norm_corpus_bigrams]
+        self._bow_corpus = [self._dictionary.doc2bow(text) for text in self._norm_corpus_bigrams]
         print(self._bow_corpus[1][:50])
         print('Total number of documents:', len(self._bow_corpus))
 
     def train(self):
         print('train')
-        TOTAL_TOPICS = 10
+        TOTAL_TOPICS = 100
         lda_model = gensim.models.LdaModel(corpus=self._bow_corpus, id2word=self._dictionary, chunksize=1740, 
                                         alpha='auto', eta='auto', random_state=42,
                                         iterations=500, num_topics=TOTAL_TOPICS, 
                                         passes=20, eval_every=None)
-        for topic_id, topic in lda_model.print_topics(num_topics=10, num_words=20):
+        for topic_id, topic in lda_model.print_topics(num_topics=TOTAL_TOPICS, num_words=20):
             print('Topic #'+str(topic_id+1)+':')
             print(topic)
             print()                                        
         # model quality
-        cv_coherence_model_lda = gensim.models.CoherenceModel(model=lda_model, corpus=bow_corpus, 
-                                                            texts=norm_corpus_bigrams,
-                                                            dictionary=dictionary, 
+        cv_coherence_model_lda = gensim.models.CoherenceModel(model=lda_model, corpus=self._bow_corpus, 
+                                                            texts=self._norm_corpus_bigrams,
+                                                            dictionary=self._dictionary, 
                                                             coherence='c_v')
         avg_coherence_cv = cv_coherence_model_lda.get_coherence()
 
-        umass_coherence_model_lda = gensim.models.CoherenceModel(model=lda_model, corpus=bow_corpus, 
-                                                                texts=norm_corpus_bigrams,
-                                                                dictionary=dictionary, 
+        umass_coherence_model_lda = gensim.models.CoherenceModel(model=lda_model, corpus=self._bow_corpus, 
+                                                                texts=self._norm_corpus_bigrams,
+                                                                dictionary=self._dictionary, 
                                                                 coherence='u_mass')
         avg_coherence_umass = umass_coherence_model_lda.get_coherence()
 
-        perplexity = lda_model.log_perplexity(bow_corpus)
+        perplexity = lda_model.log_perplexity(self._bow_corpus)
 
         print('Avg. Coherence Score (Cv):', avg_coherence_cv)
         print('Avg. Coherence Score (UMass):', avg_coherence_umass)
