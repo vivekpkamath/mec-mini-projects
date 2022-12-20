b610bfe9af28c34fc2f66a6cdf2d7a6d246271f0
vivekpkamath <vivek.p.kamath@gmail.com>
2022-12-20 06:31:35 -0800

    Added api code

--- capstone/api/model_api.py
+++ capstone/api/model_api.py
@@ -1,6 +1,12 @@
 import sys
 sys.path.append('../sql')
+sys.path.append('../model')
+
 from DAL import DAL 
+from TopicModel import TopicModel
+from BERTTopiceModel import BERTTopicModel
+from AFINNSentimentModel import AFINNSentimentModel
+from VADERSentimentModel import VADERSentimentModel
 
 from flask import Flask
 from flask import jsonify
@@ -49,30 +55,91 @@ def model_job():
 #
 @app.route('/model/', methods=['GET'])
 def model():
-    return request.json
+    if request.method == 'GET':
+        #Get all jobs from the database and send them back to the client
+        
+        dal = DAL()
+        models = dal.getAllCurrentModels()
+
+        return json.dumps( models,default=str ), 200, {'Content-Type':'application/json'} 
+    else:
+        Flask.abort(404, 'Not Supported')
+
+
 #
 # API to to get list of model metrics for a given trained model
 # 
 @app.route('/model/metrics/', methods=['GET'])
-def model_metrics(id):
-    return request.json
+def model_metrics(model_id):
+    #get all metrics if any for a given model
+    if request.method == 'GET':
+        #Get all jobs from the database and send them back to the client
+        
+        if model_id is None:
+            Flask.abort(404, 'Not Supported')
+        else:
+            dal = DAL()
+            metrics = dal.getModelMetrics(model_id)
+
+            return json.dumps( metrics,default=str ), 200, {'Content-Type':'application/json'} 
+    else:
+        Flask.abort(404, 'Not Supported')
+
 
 #
 # document related APIs
 #
 #
-# API to score topic on unseen document
+# API to score topic on unseen document with current model for given type
 #
 @app.route('/document/topic/', methods=['POST'])
-def document_topic(unseen_doc):
-    return request.json
+def document_topic():
+
+    if request.method == 'POST':
+        
+        data = request.get_json()
+        dal = DAL()
+        # Move this to a factory
+        model_type = data['model_type']
+        if model_type == 'topicLDA':
+            model = TopicModel()
+        elif model_type == 'BERTopic':
+            model = BERTTopicModel()
+        else:
+            Flask.abort(404, 'Not Supported')
+            return    
+
+        prediction = model.predict(data['unseen_document'])
+
+        return json.dumps( prediction,default=str ), 200, {'Content-Type':'application/json'} 
+    else:
+        Flask.abort(404, 'Not Supported')
 
 #
 # API to score sentiment on unseen document
 #
 @app.route('/document/sentiment/', methods=['POST'])
 def document_sentiment(unseen_doc):
-    return request.json
+    if request.method == 'POST':
+        
+        data = request.get_json()
+        dal = DAL()
+        # Move this to a factory
+        model_type = data['model_type']
+
+        if model_type == 'AFINNSentiment':
+            model = AFINNSentimentModel()
+        elif model_type == 'VADERSentiment':
+            model = VADERSentimentModel()
+        else:
+            Flask.abort(404, 'Not Supported')
+            return    
+
+        prediction = model.predict(data['unseen_document'])
+
+        return json.dumps( prediction,default=str ), 200, {'Content-Type':'application/json'} 
+    else:
+        Flask.abort(404, 'Not Supported')
 
 if __name__ == '__main__':
 
--- capstone/api/tests/runtests.sh
+++ capstone/api/tests/runtests.sh
@@ -1,3 +1,8 @@
 curl -X GET -H "Content-type: application/json" -H "Accept: application/json" http://localhost:8080/model_job/
+echo -e "\n\n"
 curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d '{"job_type":"1","model_type":"topicLDA", "comment":"test"}'   http://localhost:8080/model_job/
+curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d '{"job_type":"1","model_type":"BERTopic", "comment":"test"}'   http://localhost:8080/model_job/
+curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d '{"job_type":"2","model_type":"AFINNSentiment", "comment":"test"}'   http://localhost:8080/model_job/
+curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d '{"job_type":"2","model_type":"VADERSentiment", "comment":"test"}'   http://localhost:8080/model_job/
+echo -e "\n\n"
 curl -X GET -H "Content-type: application/json" -H "Accept: application/json" http://localhost:8080/model_job/
